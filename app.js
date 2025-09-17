d6aada69bca7b368afb8c4d37f31500b1405a3d6
Luís Gustavo <gustavo8godoy@outlook.com.br>
2025-09-17 17:31:45 -0300

    Adicionando as funções para atualizar a lista e para limpar o input

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ function adicionarAmigo() {
     limparCampo();
     atualizarLista();
 }
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li"); 
+    item.textContent = amigos[i]; 
+    lista.appendChild(item); 
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
